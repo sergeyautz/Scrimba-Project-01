fce661ae98a828668b3967da66a2982f1bc7418a
Sergey Auts <sergeyauts@gmail.com>
2024-02-07 00:00:07 +0300

    Some changes

---
+++ index.js
@@ -0,0 +1,20 @@
+let count = 0
+
+function increment() {
+    count += 1
+    document.getElementById("count-el").innerText = count
+}
+
+function save() {
+
+    document.getElementById("saved-el").textContent += count + '-'
+    count = 0
+    document.getElementById("count-el").innerText = count
+
+}
+
+function clearMe() {
+
+    document.getElementById("saved-el").textContent = ' '
+}
+
